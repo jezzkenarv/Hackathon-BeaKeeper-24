235de37cd9b45076f7c6281480f7a86eee8d0e0c
jessicajung1251 <jessica.yj.jung@gmail.com>
2024-07-13 08:44:40 +0200

    Initial commit

---
+++ script/BeaconProof.s.sol
@@ -0,0 +1,10 @@
+// SPDX-License-Identifier: GPL-3.0
+
+pragma solidity ^0.8.18;
+
+import {stdJson} from "forge-std/StdJson.sol";
+import {Script} "forge-std/Script.sol";
+
+contract ProposerBlockProof is Script {
+    
+}
---
+++ src/BeaconProof.sol
@@ -0,0 +1,9 @@
+// SPDX-License-Identifier: GPL-3.0
+
+pragma solidity ^0.8.18;
+
+import {stdJson} from "forge-std/StdJson.sol";
+
+contract ProposerBlockProof {
+    
+}
